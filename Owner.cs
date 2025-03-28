72fafa9915055c8ac6da10f453863d3fdef27ca2
G-Bence <gemesbence007@gmail.com>
2025-03-28 10:50:53 +0100

    Owner Class [Done]

---
+++ Owner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal sealed class Owner
+    {
+       public string Owners_Name { get; set; }
+       
+       // public string OwnersName { get => Owners_Name; set => Owners_Name = value; }
+    }
+}
